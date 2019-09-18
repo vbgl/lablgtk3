806a89401fc0d5559e28f61f3a38143f79e1d11f
Vincent Laporte <Vincent.Laporte@gmail.com>
2019-09-18 14:51:30 +0000

    Add missing Cocoa dependency on darwin

--- default.nix
+++ default.nix
@@ -8,6 +8,7 @@ pname = "example3";
 version = "0.0";
 src = ./.;
 
-buildInputs = [ lablgtk3-sourceview3 ];
+buildInputs = [ lablgtk3-sourceview3 ]
+++ stdenv.lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.Cocoa;
 
 }
